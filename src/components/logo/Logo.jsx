b2bd797b6aab3f3b22b1040e7c5cbce1f967ab69
Adam Dziendziel <fadehelix@gmail.com>
2017-06-30 17:13:53 +0200

    New svg logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import LogoImage from './img/logo.png';
+import LogoImage from './img/head.svg';
 const Logo = ( alt="Nacotic logo" )  => {
+  console.log(LogoImage)
   return(
     <img src={LogoImage} alt={alt} />
   );
